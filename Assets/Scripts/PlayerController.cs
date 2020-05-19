cb8171fb49f2e0f10bb0573baf532023ee8fc862
BrandynR <brandynr@github.com>
2020-05-19 12:28:04 -0400

    Initial push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+    private int score = 0;
+    public int health = 5;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            score += 1;
+            Debug.Log("Score: " + score);
+        }
+        else if (other.CompareTag("Trap"))
+        {
+            health -= 1;
+            Debug.Log("Health: " + health);
+        }
+        else if (other.CompareTag("Goal"))
+        {
+        Debug.Log("You win!");
+        }
+    }
+     void Update()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene("maze");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 0x03-unity-ui
-Adding UI to MAZE game
+# 0x02. Unity - Scripting
+Creating a Maze game where you collect coins while avoiding traps, until you reacg the goal.
